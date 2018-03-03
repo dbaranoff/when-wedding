d615299a169f01e0012f0c0ccd98adb053efc3d1
Баранов Дмитрий <cps.baranoff@gmail.com>
2018-03-03 11:57:35 +0200

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js');
+
+window.addEventListener('load', function (event) {
+    calc();
+});
+
+window.addEventListener('resize', function (event) {
+    calc();
+});
+
+function calc() {
+    var dateFrom = new Date(2018, 2, 1);
+    var dateUntil = new Date(2018, 3, 20);
+    var date = new Date();
+
+    var timeLabel = document.getElementById('time');
+    var progressLine = document.getElementById('progress-line');
+    var timeLine = document.getElementById('time-line');
+
+    var fullDistance = dateUntil - dateFrom;
+
+    var piece = dateUntil - date;
+
+    var progress = piece/fullDistance;
+
+    progressLine.style.width = timeLine.clientWidth * progress + 'px';
+
+    var remaining = Math.ceil(piece /(1000*60*60*24));
+
+    timeLabel.innerHTML = (remaining);
+
+    // console.dir({
+    //     'dateFrom': dateFrom,
+    //     'dateUntil': dateUntil,
+    //     'date': date,
+    //     'fullDistance': fullDistance,
+    //     'piece': piece,
+    //     'progress': progress,
+    //     'progressLine.width': progressLine.style.width,
+    //     'timeline.width': timeLine.clientWidth
+    // });
+    // console.dir(timeLine);
+}
---
+++ sw.js
@@ -0,0 +1,39 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('static')
+            .then(function(cache) {
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/manifest.json',
+                    '/src/css/app.css',
+                    '/src/images/bride.jpg',
+                    '/src/images/bridegroom.jpg',
+                    '/src/images/icons/icon-96x96.png',
+                    '/src/images/icons/icon-144x144.png',
+                    '/src/images/icons/icon-256x256.png',
+                    '/src/images/icons/icon-512x512.png',
+                    '/src/js/app.js',
+                    'https://fonts.googleapis.com/css?family=Raleway:400,700'
+                ]);
+            })
+    );
+});
+
+self.addEventListener('activate', function() {
+
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(res) {
+                if (res) {
+                    return res;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    );
+});
+
