14b16ffbc7cffea49ec7c39b66280e76bf381b53
Dima <baranoff_1942@mail.ru>
2018-03-03 12:05:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,17 +3,17 @@ self.addEventListener('install', function(event) {
         caches.open('static')
             .then(function(cache) {
                 cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/manifest.json',
-                    '/src/css/app.css',
-                    '/src/images/bride.jpg',
-                    '/src/images/bridegroom.jpg',
-                    '/src/images/icons/icon-96x96.png',
-                    '/src/images/icons/icon-144x144.png',
-                    '/src/images/icons/icon-256x256.png',
-                    '/src/images/icons/icon-512x512.png',
-                    '/src/js/app.js',
+                    './',
+                    './index.html',
+                    './manifest.json',
+                    './src/css/app.css',
+                    './src/images/bride.jpg',
+                    './src/images/bridegroom.jpg',
+                    './src/images/icons/icon-96x96.png',
+                    './src/images/icons/icon-144x144.png',
+                    './src/images/icons/icon-256x256.png',
+                    './src/images/icons/icon-512x512.png',
+                    './src/js/app.js',
                     'https://fonts.googleapis.com/css?family=Raleway:400,700'
                 ]);
             })
